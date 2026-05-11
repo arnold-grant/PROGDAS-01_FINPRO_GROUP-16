a5cd0cfb8f0cb0f094d3c4215bdd21bac8b00d71
grantzsecond-dotcom <grantzsecond@gmail.com>
2026-05-11 22:04:44 +0700

    Template Main.c

---
+++ Desktop/MAIN-PROJECT/Main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_MAKANAN 50
+
+typedef enum {
+
+} StatusKelayakan;
+
+typedef union {
+
+} InfoTambahan;
+
+typedef struct {
+
+} Makanan;
+
+int hitungSkorKarbohidrat(float gram) {
+
+}
+
+int hitungSkorProtein(float gram) {
+
+}
+
+int hitungSkorLemak(float gram) {
+
+}
+
+void hitungKomposisi(Makanan *makanan) {
+
+}
+
+void kelolosanSkorMakanan(Makanan *makanan) {
+
+}
+
+void penentuanDistribusi(Makanan *makanan) {
+
+}
+
+void inputMakanan(Makanan *makanan) {
+
+}
+
+void tampilHasilMakanan(const Makanan *makanan) {
+
+}
+
+void tampilRingkasan(Makanan *daftarMakanan, int jumlah) {
+	
+}
+
+void tampilanMenu() {
+
+}
+
+int main() {
+
+    return 0;
+}
